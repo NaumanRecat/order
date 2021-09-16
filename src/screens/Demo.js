31814ff4fab41d096f4d3091c52c21d4a09d332e
NaumanRecat <naumantariq2977@gmail.com>
2021-09-16 08:42:03 -0700

    icon done

--- src/screens/Demo.js
+++ src/screens/Demo.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 
 export class Demo extends React.Component{
     render(){
@@ -8,9 +11,16 @@ export class Demo extends React.Component{
                 flex:1,
                 backgroundColor:'#ada',
             }}>
+                <TouchableOpacity style={{
+                flex:1,
+                alignItems:'center',
+                justifyContent:'center',
+                 }}>
+                <Icon name="home" size={h('4%')} color="green" />  
                 <Text>
-                    nomi
+                    Text1
                 </Text>
+            </TouchableOpacity>
             </View>
         )
     }
